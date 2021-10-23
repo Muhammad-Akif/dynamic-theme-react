bda4acac59a8dcbc1d7714375f33c036b173061d
muhammad-akif <muhammadakif2917@gmail.com>
2021-10-23 16:56:50 +0500

    added toggler and related tags

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ function App() {
     <>
       <div id="theme-open" onClick={() => setToggler(!toggler)} className="fas fa-bars"></div>
 
+      <div className={toggler ? 'themes-container active' : 'themes-container'}>
+
+        <div id="theme-close" onClick={() => setToggler(!toggler)} className="fas fa-times"></div>
+
+        <h3>switch theme</h3>
+
+        <div className={ themeToggler ? 'theme-toggler active' : 'theme-toggler' } onClick={() => setThemeToggler(!themeToggler)}>
+          { themeToggler ? document.body.classList.add('active') : document.body.classList.remove('active')}
+          <span>light</span>
+          <span className="toggler"></span>
+          <span>dark</span>
+        </div>
+
+        <h3>pick a color</h3>
+
+        <div className="theme-colors">
+          {colors.map((color, index) => (
+            <div className="color" index={index} onClick={() => setBtnColor(color)} style={{ backgroundColor: `${color}` }}></div>
+          ))}
+        </div>
+      </div>
+
       <div className="container">
         <div className="content">
           <img src={img} alt="cat" />
