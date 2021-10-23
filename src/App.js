5f1baaee579fc5169700fadcc051f08e2ce4ca87
muhammad-akif <muhammadakif2917@gmail.com>
2021-10-23 16:55:47 +0500

    added center block

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import img from './image/kitten.jpg';
 import './App.css';
 
 function App() {
- 
 
   return (
     <>
       <div id="theme-open" onClick={() => setToggler(!toggler)} className="fas fa-bars"></div>
+
+      <div className="container">
+        <div className="content">
+          <img src={img} alt="cat" />
+          <h3>some text heading</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Praesentium, atque?</p>
+          <a href="#" className="btn"><strong>read more</strong></a>
+        </div>
+      </div>
     </>
   );
 }
